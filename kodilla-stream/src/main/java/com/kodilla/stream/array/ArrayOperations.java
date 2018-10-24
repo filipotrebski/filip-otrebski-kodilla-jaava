185d6c1fa15bac0a8e03f4fca97e8e153dc83de3
filipotrebski <otrebskifilip@gmail.com>
2018-10-24 21:51:24 +0200

    poprawa 7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,6 +1,5 @@
 package com.kodilla.stream.array;
 
-import java.util.Arrays;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
@@ -8,8 +7,9 @@ public interface ArrayOperations {
 
     static double getAverage(int[] numbers){
         IntStream.range(0,numbers.length).forEach(x -> System.out.println(numbers[x]));
-        IntStream intStream = Arrays.stream(numbers);
-        OptionalDouble average = intStream.average();
+        //IntStream intStream = Arrays.stream(numbers);
+        OptionalDouble average = IntStream.range(0,numbers.length).map(x -> numbers[x] ).average();
+        ;
         return average.getAsDouble();
     }
 }
