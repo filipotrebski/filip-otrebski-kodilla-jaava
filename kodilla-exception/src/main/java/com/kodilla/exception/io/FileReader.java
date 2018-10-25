8d004a99c677d23a3ede9077eb0c4566b9b7633c
filipotrebski <otrebskifilip@gmail.com>
2018-10-25 20:52:43 +0200

    Moduł 8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        //Path path = Paths.get(file.getPath());
+        try (Stream<String> fileLines = Files.lines(Paths.get("files/ddd.txt"))) {
+
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+
+            System.out.println("Oh no! Something went wrong. Error :" + e);
+
+        } finally {
+
+            System.out.println("I'm gonna be here... always");
+        }
+
+        System.out.println(file.getPath());
+    }
+}
