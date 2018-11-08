56324838f61003410c8581aa3ba903b0220b104c
filipotrebski <otrebskifilip@gmail.com>
2018-11-08 15:10:36 +0100

    10.1

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/SpringIntoRunner.java
@@ -0,0 +1,7 @@
+package com.kodilla.spring.intro;
+
+public class SpringIntoRunner {
+    public static void main(String[] args) {
+        System.out.println("Welcome into the intro to spring");
+    }
+}
