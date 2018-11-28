8cefd4512f26ac6d69ba7f8f786a1044310a1dcc
filipotrebski <otrebskifilip@gmail.com>
2018-11-28 12:22:14 +0100

    Moduł 13.1

---
+++ kodilla-hibernate/src/main/java/com/example/kodillahibernate/KodillaHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.example.kodillahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+}
